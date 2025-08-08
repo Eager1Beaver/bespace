42ba57da18903660fc99fb9d0f1eba1122e901ab
Eager1Beaver <i1got1power@gmail.com>
2025-08-08 03:11:35 +0300

    updated config

--- src/config_loader.py
+++ src/config_loader.py
@@ -2,7 +2,7 @@
 from omegaconf import OmegaConf
 import os
 
-def load_config(config_path="config/config.yaml"):
+def load_config(config_path="src/config/config.yaml"):
     if not os.path.isfile(config_path):
         raise FileNotFoundError(f"Config file not found at: {config_path}")
     return OmegaConf.load(config_path)
